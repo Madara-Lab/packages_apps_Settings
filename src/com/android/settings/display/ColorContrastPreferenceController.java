DerpFest-AOSP/android_packages_apps_Settings

Settings: Introduce APIs for advanced picture color adjustments (Hue, Contrast, Brightness) [2/2]
Signed-off-by: kenway214 <kenway214@outlook.com>

src/com/android/settings/display/ColorContrastPreferenceController.java
@@ -1,0 +1,51 @@
+/*
+ * SPDX-FileCopyrightText: 2026 kenway214
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.display;
+
+import android.content.Context;
+import android.hardware.display.ColorDisplayManager;
+
+import androidx.preference.Preference;
+
+import com.android.settings.core.BasePreferenceController;
+import org.derpfest.support.preferences.ProperSeekBarPreference;
+
+public class ColorContrastPreferenceController extends BasePreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private final ColorDisplayManager mColorDisplayManager;
+
+    public ColorContrastPreferenceController(Context context, String key) {
+        super(context, key);
+        mColorDisplayManager = context.getSystemService(ColorDisplayManager.class);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return ColorDisplayManager.isColorTransformAccelerated(mContext)
+                ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return mColorDisplayManager.setColorContrast((int) newValue);
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        ((ProperSeekBarPreference) preference).setValue(mColorDisplayManager.getColorContrast());
+    }
+
+    @Override
+    public boolean isSliceable() {
+        return true;
+    }
+
+    @Override
+    public boolean isPublicSlice() {
+        return true;
+    }
+}

src/com/android/settings/display/ColorHuePreferenceController.java
@@ -1,0 +1,51 @@
+/*
+ * SPDX-FileCopyrightText: 2026 kenway214
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.display;
+
+import android.content.Context;
+import android.hardware.display.ColorDisplayManager;
+
+import androidx.preference.Preference;
+
+import com.android.settings.core.BasePreferenceController;
+import org.derpfest.support.preferences.ProperSeekBarPreference;
+
+public class ColorHuePreferenceController extends BasePreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private final ColorDisplayManager mColorDisplayManager;
+
+    public ColorHuePreferenceController(Context context, String key) {
+        super(context, key);
+        mColorDisplayManager = context.getSystemService(ColorDisplayManager.class);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return ColorDisplayManager.isColorTransformAccelerated(mContext)
+                ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return mColorDisplayManager.setColorHue((int) newValue);
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        ((ProperSeekBarPreference) preference).setValue(mColorDisplayManager.getColorHue());
+    }
+
+    @Override
+    public boolean isSliceable() {
+        return true;
+    }
+
+    @Override
+    public boolean isPublicSlice() {
+        return true;
+    }
+}

src/com/android/settings/display/ColorPictureBrightnessPreferenceController.java
@@ -1,0 +1,52 @@
+/*
+ * SPDX-FileCopyrightText: 2026 kenway214
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.display;
+
+import android.content.Context;
+import android.hardware.display.ColorDisplayManager;
+
+import androidx.preference.Preference;
+
+import com.android.settings.core.BasePreferenceController;
+import org.derpfest.support.preferences.ProperSeekBarPreference;
+
+public class ColorPictureBrightnessPreferenceController extends BasePreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private final ColorDisplayManager mColorDisplayManager;
+
+    public ColorPictureBrightnessPreferenceController(Context context, String key) {
+        super(context, key);
+        mColorDisplayManager = context.getSystemService(ColorDisplayManager.class);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return ColorDisplayManager.isColorTransformAccelerated(mContext)
+                ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return mColorDisplayManager.setColorBrightness((int) newValue);
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        ((ProperSeekBarPreference) preference).setValue(
+                mColorDisplayManager.getColorBrightness());
+    }
+
+    @Override
+    public boolean isSliceable() {
+        return true;
+    }
+
+    @Override
+    public boolean isPublicSlice() {
+        return true;
+    }
+}

Madara-Lab/packages_apps_Settings

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: Ghosuto <clash.raja10@gmail.com>
(cherry picked from commit 737e0024b0b553333264310531dbac24e9aece29)
Signed-off-by: Madara273 <ravenhoxs@gmail.com>

src/com/android/settings/display/ColorContrastPreferenceController.java
@@ -1,0 +1,51 @@
+/*
+ * SPDX-FileCopyrightText: 2026 kenway214
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.display;
+
+import android.content.Context;
+import android.hardware.display.ColorDisplayManager;
+
+import androidx.preference.Preference;
+
+import com.android.settings.core.BasePreferenceController;
+import org.lunaris.settings.preferences.CustomSeekBarPreference;
+
+public class ColorContrastPreferenceController extends BasePreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private final ColorDisplayManager mColorDisplayManager;
+
+    public ColorContrastPreferenceController(Context context, String key) {
+        super(context, key);
+        mColorDisplayManager = context.getSystemService(ColorDisplayManager.class);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return ColorDisplayManager.isColorTransformAccelerated(mContext)
+                ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return mColorDisplayManager.setColorContrast((int) newValue);
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        ((CustomSeekBarPreference) preference).setValue(mColorDisplayManager.getColorContrast());
+    }
+
+    @Override
+    public boolean isSliceable() {
+        return true;
+    }
+
+    @Override
+    public boolean isPublicSlice() {
+        return true;
+    }
+}

src/com/android/settings/display/ColorHuePreferenceController.java
@@ -1,0 +1,51 @@
+/*
+ * SPDX-FileCopyrightText: 2026 kenway214
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.display;
+
+import android.content.Context;
+import android.hardware.display.ColorDisplayManager;
+
+import androidx.preference.Preference;
+
+import com.android.settings.core.BasePreferenceController;
+import org.lunaris.settings.preferences.CustomSeekBarPreference;
+
+public class ColorHuePreferenceController extends BasePreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private final ColorDisplayManager mColorDisplayManager;
+
+    public ColorHuePreferenceController(Context context, String key) {
+        super(context, key);
+        mColorDisplayManager = context.getSystemService(ColorDisplayManager.class);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return ColorDisplayManager.isColorTransformAccelerated(mContext)
+                ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return mColorDisplayManager.setColorHue((int) newValue);
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        ((CustomSeekBarPreference) preference).setValue(mColorDisplayManager.getColorHue());
+    }
+
+    @Override
+    public boolean isSliceable() {
+        return true;
+    }
+
+    @Override
+    public boolean isPublicSlice() {
+        return true;
+    }
+}

src/com/android/settings/display/ColorPictureBrightnessPreferenceController.java
@@ -1,0 +1,52 @@
+/*
+ * SPDX-FileCopyrightText: 2026 kenway214
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.display;
+
+import android.content.Context;
+import android.hardware.display.ColorDisplayManager;
+
+import androidx.preference.Preference;
+
+import com.android.settings.core.BasePreferenceController;
+import org.lunaris.settings.preferences.CustomSeekBarPreference;
+
+public class ColorPictureBrightnessPreferenceController extends BasePreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private final ColorDisplayManager mColorDisplayManager;
+
+    public ColorPictureBrightnessPreferenceController(Context context, String key) {
+        super(context, key);
+        mColorDisplayManager = context.getSystemService(ColorDisplayManager.class);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return ColorDisplayManager.isColorTransformAccelerated(mContext)
+                ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return mColorDisplayManager.setColorBrightness((int) newValue);
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        ((CustomSeekBarPreference) preference).setValue(
+                mColorDisplayManager.getColorBrightness());
+    }
+
+    @Override
+    public boolean isSliceable() {
+        return true;
+    }
+
+    @Override
+    public boolean isPublicSlice() {
+        return true;
+    }
+}